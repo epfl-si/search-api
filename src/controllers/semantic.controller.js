04c31188f1c28f75a52480f533b17162cc43964a
William Belle <william.belle@gmail.com>
2023-05-22 16:26:52 +0200

    Add cache to Semantic search (graphsearch) (#38)

--- src/controllers/semantic.controller.js
+++ src/controllers/semantic.controller.js
@@ -1,15 +1,21 @@
+const appCache = require('../services/cache.service');
 const semanticService = require('../services/semantic.service');
 
 async function post (req, res) {
-  try {
-    const results = await semanticService.post(req.query);
-    return res.json(results.data);
-  } catch (err) {
-    console.error('[error] ', err.message);
-    return res.status(400).json({
-      success: false,
-      error: 'Oops, something went wrong'
-    });
+  if (appCache.has(req.originalUrl)) {
+    return res.send(appCache.get(req.originalUrl));
+  } else {
+    try {
+      const results = await semanticService.post(req.query);
+      appCache.set(req.originalUrl, results.data);
+      return res.json(results.data);
+    } catch (err) {
+      console.error('[error] ', err.message);
+      return res.status(400).json({
+        success: false,
+        error: 'Oops, something went wrong'
+      });
+    }
   }
 }
 
