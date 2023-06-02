89c8b188d7b837e1423176ef154c38b3b5e1d80a
Olivier Bieler <olivier.bieler@epfl.ch>
2023-06-02 11:58:51 +0200

    Exit if any keys is missing from cadidb.config + add tests

--- src/configs/cadidb.config.js
+++ src/configs/cadidb.config.js
@@ -9,4 +9,39 @@ const cadidb = {
   }
 };
 
+if (!cadidb.db.host) {
+  console.error(
+    '[error] The "SEARCH_API_CADIDB_HOST" environment variable is required'
+  );
+  process.exit(1);
+}
+
+if (!cadidb.db.user) {
+  console.error(
+    '[error] The "SEARCH_API_CADIDB_USER" environment variable is required'
+  );
+  process.exit(1);
+}
+
+if (!cadidb.db.password) {
+  console.error(
+    '[error] The "SEARCH_API_CADIDB_PASSWORD" environment variable is required'
+  );
+  process.exit(1);
+}
+
+if (!cadidb.db.database) {
+  console.error(
+    '[error] The "SEARCH_API_CADIDB_DATABASE" environment variable is required'
+  );
+  process.exit(1);
+}
+
+if (!cadidb.db.port) {
+  console.error(
+    '[error] The "SEARCH_API_CADIDB_PORT" environment variable is required'
+  );
+  process.exit(1);
+}
+
 module.exports = cadidb;
