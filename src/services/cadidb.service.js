f47676d591be91006f7de0b47cb9de21e2afc19c
Olivier Bieler <olivier.bieler@epfl.ch>
2023-06-06 14:37:50 +0200

    Move pool.promise(..)

--- src/services/cadidb.service.js
+++ src/services/cadidb.service.js
@@ -3,17 +3,17 @@ const cadidbConfig = require('../configs/cadidb.config');
 
 // Create the pool
 const pool = mysql.createPool(cadidbConfig.db);
+// Get a Promise wrapped instance of the pool
+const promisePool = pool.promise();
 
 async function sendQuery (query, values) {
   try {
-    // Get a Promise wrapped instance of the pool
-    const promisePool = pool.promise();
     // Perform the database query
     const [rows] = await promisePool.query(query, values);
-    return (rows);
+    return rows;
   } catch (err) {
     console.error('Error executing query', err);
-    return (err);
+    return err;
   }
 }
 
