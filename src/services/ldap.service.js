bf871f83ff71e42e520f583766f93d913d0701ad
William Belle <william.belle@gmail.com>
2023-06-06 15:05:48 +0200

    searchAll

--- src/services/ldap.service.js
+++ src/services/ldap.service.js
@@ -4,4 +4,36 @@ const client = ldap.createClient({
   url: 'ldaps://ldap.epfl.ch'
 });
 
-module.exports = client;
+function searchAll (base, options) {
+  return new Promise((resolve, reject) => {
+    client.search(base, options, (err, res) => {
+      if (err) {
+        console.error('[error] ' + err.message);
+        reject(err);
+      }
+      const listObj = [];
+
+      res.on('searchEntry', (entry) => {
+        listObj.push(entry.pojo);
+      });
+
+      res.on('timeout', (err) => {
+        console.error('[error] ' + err.message);
+        reject(err);
+      });
+
+      res.on('error', (err) => {
+        console.error('[error] ' + err.message);
+        reject(err);
+      });
+
+      res.on('end', () => {
+        resolve(listObj);
+      });
+    });
+  });
+}
+
+module.exports = {
+  searchAll
+};
