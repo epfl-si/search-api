1b5cdf9fe25cabb8efffa843dac6ac5d7f2c969b
William Belle <william.belle@gmail.com>
2023-06-08 14:00:14 +0200

    Test

--- src/controllers/people.controller.js
+++ src/controllers/people.controller.js
@@ -1,8 +1,13 @@
 const peopleService = require('../services/people.service');
 
 async function get (req, res) {
-  const q = req.query.q || '';
+  if (!('q' in req.query)) {
+    const queryErr = 'Query is missing';
+    console.error('[error] ', queryErr);
+    return res.status(400).json({ success: false, error: queryErr });
+  }
 
+  const q = req.query.q;
   if (q.length < 2) {
     return res.json([]);
   }
