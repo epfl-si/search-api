f792b47d35c9a9ff39631474f164dc584afe7b37
William Belle <william.belle@gmail.com>
2023-06-19 16:00:21 +0200

    Clean

--- src/services/people.service.js
+++ src/services/people.service.js
@@ -1,9 +1,9 @@
 const ldapService = require('../services/ldap.service');
 
 function getPersonBySciper (sciper) {
-  const baseFi = '&(objectClass=Person)(!(employeeType=Ignore)';
+  const baseFi = '&(objectClass=Person)(!(employeeType=Ignore))';
   const opts = {
-    filter: `(${baseFi})(uniqueIdentifier=${sciper}))`,
+    filter: `(${baseFi}(uniqueIdentifier=${sciper}))`,
     scope: 'sub'
   };
   return ldapService.searchAll('c=ch', opts);
