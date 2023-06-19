fadc17a9fdb4353aa163d7ca4276a44177b3dce0
William Belle <william.belle@gmail.com>
2023-06-19 15:53:41 +0200

    Limit for now

--- src/services/people.service.js
+++ src/services/people.service.js
@@ -1,8 +1,9 @@
 const ldapService = require('../services/ldap.service');
 
 function getPersonBySciper (sciper) {
+  const baseFi = '&(objectClass=Person)(!(employeeType=Ignore)';
   const opts = {
-    filter: '(&(objectClass=Person)(uniqueIdentifier=' + sciper + '))',
+    filter: `(${baseFi})(uniqueIdentifier=${sciper}))`,
     scope: 'sub'
   };
   return ldapService.searchAll('c=ch', opts);
