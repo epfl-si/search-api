a57769738e2e954de2b9fdfb6d21608775aead34
William Belle <william.belle@gmail.com>
2023-07-13 10:33:53 +0200

    Add documentation for helper.util module

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -1,3 +1,19 @@
+/**
+ * Utilities.
+ *
+ * @module utils/helper
+ */
+
+/**
+ * Validate environment variables.
+ *
+ * @example
+ * const helper = require('../utils/helper.util');
+ * helper.validateEnv('SEARCH_API_LDAP_URL');  // => 'ldaps://ldap.epfl.ch'
+ *
+ * @param {string} key A key of an environment variable.
+ * @returns {string} Return the value or exit(1) if the key didn't exists.
+ */
 function validateEnv (key) {
   if (!process.env[key]) {
     console.error(
