ab12b2436938fdc30d1e0fb51e34182bf82151d8
Azecko <sami.panseri@epfl.ch>
2023-08-28 10:40:47 +0200

    Cache on `/api/address`

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,5 +1,6 @@
 const ldapUtil = require('../utils/ldap.util');
 const addressService = require('../services/people.service');
+const appCache = require('../services/cache.service');
 
 async function get (req, res) {
   const q = req.query.q || '';
@@ -7,15 +8,21 @@ async function get (req, res) {
     return res.json([]);
   }
 
-  try {
-    const ldapResults = await addressService.getPersonBySciper(q);
-    return res.json(ldapUtil.ldapAddress2api(ldapResults));
-  } catch (err) {
-    console.error('[error] ', err.message);
-    return res.status(400).json({
-      success: false,
-      error: 'Oops, something went wrong'
-    });
+  if (appCache.has(req.originalUrl)) {
+    return res.send(appCache.get(req.originalUrl));
+  } else {
+    try {
+      const ldapResults = await addressService.getPersonBySciper(q);
+      const jsonResponse = ldapUtil.ldapAddress2api(ldapResults);
+      appCache.set(req.originalUrl, jsonResponse);
+      return res.json(jsonResponse);
+    } catch (err) {
+      console.error('[error] ', err.message);
+      return res.status(400).json({
+        success: false,
+        error: 'Oops, something went wrong'
+      });
+    }
   }
 }
 
