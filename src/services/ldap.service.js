2794bf52ea4550de2bf308129daee2eecb5b975e
William Belle <william.belle@gmail.com>
2023-08-28 11:12:49 +0200

    ldap client

--- src/services/ldap.service.js
+++ src/services/ldap.service.js
@@ -1,9 +1,6 @@
 const ldap = require('ldapjs');
 const ldapConfig = require('../configs/ldap.config');
 
-// http://ldapjs.org/client.html
-const client = ldap.createClient(ldapConfig.client);
-
 function getSciper (entry) {
   const uids = entry.attributes.filter(
     f => f.type === 'uniqueIdentifier'
@@ -12,6 +9,9 @@ function getSciper (entry) {
 }
 
 function searchAll (base, options) {
+  // http://ldapjs.org/client.html
+  const client = ldap.createClient(ldapConfig.client);
+
   return new Promise((resolve, reject) => {
     client.search(base, options, (x, res) => {
       const personsBySciper = {};
@@ -26,15 +26,18 @@ function searchAll (base, options) {
 
       res.on('timeout', (err) => {
         console.error('[error] ' + err.message);
+        client.destroy();
         reject(err);
       });
 
       res.on('error', (err) => {
         console.error('[error] ' + err.message);
+        client.destroy();
         reject(err);
       });
 
       res.on('end', () => {
+        client.destroy();
         resolve(personsBySciper);
       });
     });
