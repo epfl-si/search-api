3571692a6d284a10d13c7ed4a83804dab79794cc
Olivier Bieler <olivier.bieler@epfl.ch>
2023-12-14 16:12:51 +0100

    Implement unit-people CSV export

--- src/controllers/unit.controller.js
+++ src/controllers/unit.controller.js
@@ -14,6 +14,29 @@ async function get (req, res) {
   }
 }
 
+async function getCsv (req, res) {
+  try {
+    const isInternal = req.header('X-EPFL-Internal') === 'TRUE';
+    if (!isInternal) {
+      return res.sendStatus(403);
+    }
+    const csvData = await unitService.getCsv(req.query);
+    if (!csvData) {
+      return res.sendStatus(404);
+    }
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename=extract.csv');
+    return res.send(csvData);
+  } catch (err) {
+    console.error('[error] ', err);
+    return res.status(400).json({
+      success: false,
+      error: 'Oops, something went wrong'
+    });
+  }
+}
+
 module.exports = {
-  get
+  get,
+  getCsv
 };
