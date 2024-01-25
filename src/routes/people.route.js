73f1f44d8769b5d5973edb9484fab368f417bce8
William Belle <william.belle@gmail.com>
2024-01-25 13:17:36 +0100

    Add route

--- src/routes/people.route.js
+++ src/routes/people.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const peopleController = require('../controllers/people.controller');
 
 router.get('/', peopleController.get);
+router.get('/suggestions/', peopleController.getSuggestions);
 
 module.exports = router;
