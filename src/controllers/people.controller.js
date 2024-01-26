690601d41c0dd270afd77565e75d67a8eb3be6a3
William Belle <william.belle@gmail.com>
2024-01-26 13:05:36 +0100

    Add cache to people suggestions (#141)

--- src/controllers/people.controller.js
+++ src/controllers/people.controller.js
@@ -1,4 +1,5 @@
 const ldapUtil = require('../utils/ldap.util');
+const appCache = require('../services/cache.service');
 const peopleService = require('../services/people.service');
 
 async function get (req, res) {
@@ -29,29 +30,34 @@ async function get (req, res) {
 }
 
 async function getSuggestions (req, res) {
-  // Max and default 10 suggestions
-  let limit = req.query.limit || 10;
-  limit = limit > 10 ? 10 : limit;
+  if (appCache.has(req.originalUrl)) {
+    return res.send(appCache.get(req.originalUrl));
+  } else {
+    // Max and default 10 suggestions
+    let limit = req.query.limit || 10;
+    limit = limit > 10 ? 10 : limit;
 
-  const q = req.query.q || '';
-  if (q.length < 2) {
-    return res.json([q, []]);
-  }
-
-  try {
-    const ldapResults = await peopleService.getPersonByName(q);
-    const results = ldapUtil.ldap2api(ldapResults, q, 'en').slice(0, limit);
-    const suggestions = [];
-    for (const person of results) {
-      suggestions.push(`${person.firstname} ${person.name}`);
+    const q = req.query.q || '';
+    if (q.length < 2) {
+      return res.json([q, []]);
     }
-    return res.json([q, suggestions]);
-  } catch (err) {
-    console.error('[error] ', err.message);
-    return res.status(400).json({
-      success: false,
-      error: 'Oops, something went wrong'
-    });
+
+    try {
+      const ldapResults = await peopleService.getPersonByName(q);
+      const results = ldapUtil.ldap2api(ldapResults, q, 'en').slice(0, limit);
+      const suggestions = [];
+      for (const person of results) {
+        suggestions.push(`${person.firstname} ${person.name}`);
+      }
+      appCache.set(req.originalUrl, [q, suggestions]);
+      return res.json([q, suggestions]);
+    } catch (err) {
+      console.error('[error] ', err.message);
+      return res.status(400).json({
+        success: false,
+        error: 'Oops, something went wrong'
+      });
+    }
   }
 }
 
