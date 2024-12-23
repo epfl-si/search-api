a3d504bd166a2b0ad4fe6e516db659c48cab6583
William Belle <william.belle@gmail.com>
2024-12-23 13:03:11 +0100

    v2

--- src/controllers/semantic.controller.js
+++ src/controllers/semantic.controller.js
@@ -19,6 +19,25 @@ async function get (req, res) {
   }
 }
 
+async function getv2 (req, res) {
+  if (appCache.has(req.originalUrl)) {
+    return res.send(appCache.get(req.originalUrl));
+  } else {
+    try {
+      const results = await semanticService.getv2(req.query);
+      appCache.set(req.originalUrl, results.data);
+      return res.json(results.data);
+    } catch (err) {
+      console.error('[error] ', err.message);
+      return res.status(400).json({
+        success: false,
+        error: 'Oops, something went wrong'
+      });
+    }
+  }
+}
+
 module.exports = {
-  get
+  get,
+  getv2
 };
--- src/services/semantic.service.js
+++ src/services/semantic.service.js
@@ -13,6 +13,20 @@ async function get (query) {
   });
 }
 
+async function getv2 (query) {
+  let limit = query.limit || 10;
+  limit = limit > 100 ? 100 : limit;
+
+  return axios.get('https://graphsearch.epfl.ch/api/v2/search/search.epfl.ch', {
+    params: {
+      q: query.q,
+      limit,
+      types: query.doctype || 'any'
+    }
+  });
+}
+
 module.exports = {
-  get
+  get,
+  getv2
 };
