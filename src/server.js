2ab21e3066a60fa770d8b8d684dc11a48c610a7f
William Belle <william.belle@gmail.com>
2025-03-17 12:48:30 +0100

    Get Prometheus Metrics

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const app = require('./app');
+const metricsApp = require('./metrics');
 const port = process.env.PORT || 5555;
+const mPort = process.env.MPORT || 5556;
 
 app.listen(port);
 console.log(
   `Server listening on 0.0.0.0 port ${port} (http://0.0.0.0:${port}/)`
+);
+
+metricsApp.listen(mPort);
+console.log(
+  `Metrics listening on 0.0.0.0 port ${mPort} (http://0.0.0.0:${mPort}/)`
 );
