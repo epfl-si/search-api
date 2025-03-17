3384c8d057d2054326a39e2133c74ef5cd49449d
William Belle <william.belle@gmail.com>
2025-03-17 13:47:06 +0100

    Logs

--- src/metrics.js
+++ src/metrics.js
@@ -1,5 +1,11 @@
+const morgan = require('morgan');
 const express = require('express');
 
 const metricsApp = express();
+
+// Log
+metricsApp.use(
+  morgan('combined', { skip: (req, res) => process.env.NODE_ENV === 'test' })
+);
 
 module.exports = metricsApp;
