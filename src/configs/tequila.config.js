77be25915e0551f0fabfec79df7639a75bbe4210
William Belle <william.belle@gmail.com>
2025-07-09 08:47:40 +0200

    Add user infos (#259)

--- src/configs/tequila.config.js
+++ src/configs/tequila.config.js
@@ -3,7 +3,7 @@ const TequilaStrategy = require('passport-tequila').Strategy;
 // Use the TequilaStrategy within Passport.
 const tequila = new TequilaStrategy({
   service: 'Search engine',
-  request: ['displayname']
+  request: ['displayname', 'uniqueid']
 });
 
 module.exports = tequila;
